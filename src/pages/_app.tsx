754b273e87612dfe0a525bda7902775ea2fae349
skyclouds2001 <skyclouds2001@163.com>
2023-01-30 13:16:18 +0800

    feat: update head-meta info

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,31 +17,31 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
     <>
       <Head>
         <meta charSet="utf-8" />
-        <meta name="application-name" content="next.js-template-sky" />
-        <meta name="author" content="skyclouds2001@163.com" />
-        <meta name="description" content="A template project for next.js by skyclouds2001." />
-        <meta name="generator" content="next.js" />
-        <meta name="keywords" content="next.js,template,config" />
+        <meta name="application-name" content="稀土掘金" />
+        <meta name="author" content="skyclouds2001@gmail.com" />
+        <meta name="description" content="掘金是面向全球中文开发者的技术内容分享与交流平台。我们通过技术文章、沸点、课程、直播等产品和服务，打造一个激发开发者创作灵感，激励开发者沉淀分享，陪伴开发者成长的综合类技术社区。" />
+        <meta name="generator" content="Next.js" />
+        <meta name="keywords" content="掘金,稀土" />
         <meta name="referrer" content="no-referrer" />
         <meta name="theme-color" media="(prefers-color-scheme:light)" content="white" />
         <meta name="theme-color" media="(prefers-color-scheme:dark)" content="black" />
         <meta name="color-schema" content="[light|dark]" />
         <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,viewport-fit=cover,user-scalable=yes" />
-        <meta name="creator" content="skyclouds2001@163.com" />
+        <meta name="creator" content="skyclouds2001@gmail.com" />
         <meta name="googlebot" content="index,follow" />
-        <meta name="publisher" content="skyclouds2001@163.com" />
+        <meta name="publisher" content="skyclouds2001@gmail.com" />
         <meta name="robots" content="index,follow" />
         <meta name="renderer" content="webkit" />
         <meta name="force-rendering" content="webkit" />
         <meta name="format-detection" content="telephone=no,date=no,address=no" />
         <meta httpEquiv="x-ua-compatible" content="IE=edge,chrome=1" />
         <meta httpEquiv="content-type" content="text/html;charset=utf-8" />
-        <meta property="og:title" content="next.js-template-sky" />
+        <meta property="og:title" content="稀土掘金" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="/" />
         <meta property="og:image" content="/favicon.ico" />
         <base href="/" target="_self" />
-        <title>next.js template</title>
+        <title>稀土掘金</title>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
