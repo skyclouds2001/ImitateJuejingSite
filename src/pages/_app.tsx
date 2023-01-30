5e02c5fd9fe05fe525ef446c58e490aca553c28b
skyclouds2001 <skyclouds2001@163.com>
2023-01-30 13:04:13 +0800

    feat: add favicon ico

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -42,6 +42,10 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
         <meta property="og:image" content="/favicon.ico" />
         <base href="/" target="_self" />
         <title>next.js template</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <ConfigProvider locale={zhCN}>
