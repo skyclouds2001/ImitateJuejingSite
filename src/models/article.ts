d905b2910672b28d581b98383b799f12c2d613b2
skyclouds2001 <skyclouds2001@163.com>
2023-02-05 12:24:17 +0800

    feat: create article model

---
+++ src/models/article.ts
@@ -0,0 +1,23 @@
+/**
+ * 文章数据结构
+ */
+interface Article {
+  /** 文章标题 */
+  title: string
+  /** 文章作者 */
+  author: string
+  /** 文章分类tab */
+  labels: string[]
+  /** 文章摘要，可选 */
+  digest: string
+  /** 文章内容 */
+  content: string
+  /** 文章发表时间 */
+  createTime: string
+  /** 阅读数，用于文章排序 */
+  readCnt: number
+  /** 图片；文章封面，可选 */
+  cover: string
+}
+
+export default Article
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,1 +1,3 @@
 export type { default as User } from './user'
+
+export type { default as Article } from './article'
