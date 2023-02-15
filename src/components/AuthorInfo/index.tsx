faffa4e9044a2ff33f518b458dfaa9a292d5b2f4
Han_ruofei <zx529213@163.com>
2023-02-15 13:39:39 +0800

    feat: 接口调试

--- src/components/AuthorInfo/index.tsx
+++ src/components/AuthorInfo/index.tsx
@@ -4,25 +4,17 @@ import Image from 'next/image'
 import { EyeTwoTone, LikeTwoTone } from '@ant-design/icons'
 import styles from './index.module.css'
 // 作者相关信息接口
-interface article {}
-interface author {
-  // readCnt: number
-  // likeUsers: object
-  // author: {
-  //   username: string
-  //   profile: object
-  //   writeLevel: number
-  // }
-}
+// interface article {}
+// interface author {
+//   id: number
+//   username: string
+//   job: string
+//   likeUsers: object
+//   readCnt: number
+//   writeLevel: number
+// }
 const AuthorInfo = (props: any) => {
-  const data: author = {
-    // id: 1,
-    // title: 'aaa',
-    // author: '小弟调调',
-    // readCnt: 2300,
-    // likeUsers: 100,
-    // authorInfo: 'bbbb',
-  }
+  // const data: author = {}
   // 一部分信息是通过props传递的，还有一部分需要另外获取
   // 通过传递的作者id获取作者的信息
   // useEffect(() => {
@@ -31,23 +23,45 @@ const AuthorInfo = (props: any) => {
   // useEffect(()=>{
   //   instance('/api/article/2')
   // },[])
+  // useEffect(() => {
+  //   instance
+  //     .get('/api/articles/1?populate=*')
+  //     .then((res) => {
+  //       console.log(res)
+  //     })
+  //     .catch((e) => {
+  //       console.log(e)
+  //     })
+  // })
+  console.log(props)
+  // http://localhost:8080/api/articles/1?populate[author][populate]=%2A
+  const authorInfo = {
+    imgUrl: 'https://p3-passport.byteimg.com/img/user-avatar/9eb2a75c3063dde89a8da01253c41386~100x100.awebp',
+    username: '小松鼠',
+    job: '前端开发工程狮',
+    writeLevel: '4',
+    readCnt: 320,
+    likeCnt: 68,
+  }
+
   return (
     <>
       <div className={styles.box}>
         {/* 作者信息头部 */}
         <a href="http:junjin.com" className={styles.head}>
           <div className={styles.avatar}>
-            <Image src="https://p3-passport.byteimg.com/img/user-avatar/9eb2a75c3063dde89a8da01253c41386~100x100.awebp" alt="" className={styles.pic} width="50" height="50" />
+            <img src={authorInfo.imgUrl} alt="头像" className={styles.pic} width="50" height="50" />
           </div>
           <div className={styles.intro}>
             <div className={styles.heads}>
-              <p className={styles.name}>小弟调调</p>
+              <p className={styles.name}>{authorInfo.username}</p>
               <p className={styles.levels}>
-                <Image src="https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/img/lv-5.d08789d.png" alt="等级" className={styles.avimg} width="35" height="16" />
+                {/* 这里的等级对应的是图片，先以默认图片代替 */}
+                <img src="https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/img/lv-5.d08789d.png" alt="等级" className={styles.avimg} width="35" height="16" />
               </p>
             </div>
 
-            <p className={styles.posts}>杂役</p>
+            <p className={styles.posts}>{authorInfo.job}</p>
           </div>
         </a>
         {/* 关注 点赞 */}
@@ -60,11 +74,11 @@ const AuthorInfo = (props: any) => {
         <div className={styles.info}>
           <p className={styles.like}>
             <EyeTwoTone />
-            <span className={styles.likes}>获得点赞</span>&nbsp;10,395
+            <span className={styles.likes}>获得点赞</span>&nbsp;{authorInfo.likeCnt}
           </p>
           <p className={styles.read}>
             <LikeTwoTone />
-            <span className={styles.reads}>文章被阅读</span>&nbsp;331,384
+            <span className={styles.reads}>文章被阅读</span>&nbsp;{authorInfo.readCnt}
           </p>
         </div>
       </div>
--- src/components/RealtedArticle/index.tsx
+++ src/components/RealtedArticle/index.tsx
@@ -1,50 +1,58 @@
+import { useEffect, useState } from 'react'
 import styles from './index.module.css'
-interface relatedArticleType {
-  title: string
-  likeCount: number
-  commentCount: number
-  link: string
-}
-const RelatedArticle = () => {
-  const relatedArticleList: Array<relatedArticleType> = [
+import instance from '@/lib/http'
+// interface relatedArticleType {
+//   title: string
+//   likeUsers: Array<any>
+//   comments: Array<any>
+//   link: string
+// }
+// 组件接收一个typeId
+const RelatedArticle = (props: number = 2) => {
+  const relatedArticleList: any = [
     {
-      title: '常用Git命令总结',
+      title: 'Git常用',
+      likeUsers: [34, 2, 5, 3, 56, 3, 23, 32, 3],
+      comments: ['34', '34', 65, 76, 34, 12],
       link: 'http://baidu.com',
-      likeCount: 300,
-      commentCount: 20,
     },
     {
-      title: 'Git常用命令',
+      title: 'Git常用',
+      likeUsers: [34, 2, 5, 3, 56, 3, 23, 32, 3],
+      comments: ['34', '34', 65, 76, 34, 12],
       link: 'http://baidu.com',
-      likeCount: 120,
-      commentCount: 20,
     },
     {
-      title: '本人自用Git',
+      title: 'Git常用',
+      likeUsers: [34, 2, 5, 3, 56, 3, 23, 32, 3],
+      comments: ['34', '34', 65, 76, 34, 12],
       link: 'http://baidu.com',
-      likeCount: 420,
-      commentCount: 14,
-    },
-    {
-      title: '你要的Git来了',
-      link: 'http://baidu.com',
-      likeCount: 236,
-      commentCount: 54,
     },
   ]
+  // useEffect(() => {
+  //   instance('/api/article-labels/2?populate[articles][populate]=%2A')
+  //     .then((res) => {
+  //       relatedArticleList = res.data.data.attributes.articles.data
+
+  //     })
+  //     .catch((e) => {
+  //       console.log(e)
+  //     })
+  // }, [])
+
   return (
     <>
       <div className={styles.box}>
         <div className={styles.title}>相关文章</div>
         <div className={styles.entry}>
-          {relatedArticleList.map((item, index) => {
+          {relatedArticleList.map((item: any, index: number) => {
             return (
-              <a className={styles.item} href={item.link} key={'artie' + index}>
+              <a className={styles.item} href={'/api/articles/' + item.id} key={'artie' + index}>
                 <div className={styles.itemtitle}>{item.title}</div>
                 <div className={styles.metabox}>
-                  <div className={styles.metalike}>{item.likeCount}点赞</div>
+                  <div className={styles.metalike}>{item.likeUsers?.length ?? 20}点赞</div>
                   <div className={styles.space}>&nbsp;·&nbsp;</div>
-                  <div className={styles.metacomment}>{item.commentCount}评论</div>
+                  <div className={styles.metacomment}>{item.comments?.length ?? 3}评论</div>
                 </div>
               </a>
             )
