a0e2abfc2936bde6c231400830de0c3886492e45
skyclouds2001 <skyclouds2001@163.com>
2023-02-17 00:38:15 +0800

    feat: update article list and support switch article tab

--- src/components/ArticleList/index.tsx
+++ src/components/ArticleList/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, type MouseEventHandler } from 'react'
 import Link from 'next/link'
 import { useArticle } from '@/api'
 import ArticleListItem from '@/components/ArticleListItem'
@@ -6,9 +6,24 @@ import styles from './index.module.scss'
 
 const ArticleList: React.FC = () => {
   const { data } = useArticle()
-  console.log(data)
 
+  /**
+   * 当前活跃 Nav
+   */
   const [currentNav, setCurrentNav] = useState(1)
+
+  /**
+   * Nav 点击事件 - 切换文章 Tab 事件
+   *
+   * @param e 鼠标点击事件
+   */
+  const switchNav: MouseEventHandler<HTMLAnchorElement> = (e) => {
+    e.preventDefault()
+    const { index } = (e.target as HTMLAnchorElement).dataset
+    if (index) {
+      setCurrentNav(parseInt(index))
+    }
+  }
 
   return (
     <>
@@ -17,19 +32,27 @@ const ArticleList: React.FC = () => {
           <nav className={styles.nav}>
             <ul className={styles.list}>
               <li className={`${styles.item} ${currentNav === 1 ? styles.active : ''}`}>
-                <Link href="/">推荐</Link>
+                <Link href="/" data-index={1} onClick={switchNav}>
+                  推荐
+                </Link>
               </li>
               <li className={`${styles.item} ${currentNav === 2 ? styles.active : ''}`}>
-                <Link href="/">最新</Link>
+                <Link href="/" data-index={2} onClick={switchNav}>
+                  最新
+                </Link>
               </li>
               <li className={`${styles.item} ${currentNav === 3 ? styles.active : ''}`}>
-                <Link href="/">热榜</Link>
+                <Link href="/" data-index={3} onClick={switchNav}>
+                  热榜
+                </Link>
               </li>
             </ul>
           </nav>
         </header>
         <article className={styles.container}>
-          <ArticleListItem />
+          {data.data.map((v) => (
+            <ArticleListItem key={v.id} article={v.attributes} />
+          ))}
         </article>
       </div>
     </>
