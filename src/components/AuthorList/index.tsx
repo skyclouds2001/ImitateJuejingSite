b918322d6201f8f283badc24ae320a83ee63cb05
skyclouds2001 <skyclouds2001@163.com>
2023-02-16 13:06:38 +0800

    feat: update sidebar

--- src/components/AuthorList/index.tsx
+++ src/components/AuthorList/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 import useSWR from 'swr'
 import type { User } from '@/models'
 import styles from './index.module.css'
@@ -6,23 +7,23 @@ import styles from './index.module.css'
 const AuthorList: React.FC = () => {
   return (
     <>
-      <div className={styles.authorList}>
-        <header className={styles.authorHeader}>🎖️作者榜</header>
-        <article className={styles.authorContent}>
-          <section className={styles.authorItem}>
-            <img className={styles.authorAvatar} src="https://p3-passport.byteimg.com/img/user-avatar/ee5b3d33c959244bf7b70b28bb3a4d07~100x100.awebp" alt={'' /* todo */} loading="lazy" title="用户头像" />
-            <a href="/user/4406498333033918" target="_blank" className={styles.authorUserinfo}>
-              <div className={styles.authorUsername}>
-                <span className={styles.authorName}>天行无忌</span>
-                <span className={styles.authorRank}>
+      <div className={styles.list}>
+        <header className={styles.header}>🎖️作者榜</header>
+        <article className={styles.content}>
+          <section className={styles.item}>
+            <img className={styles.avatar} src="https://p3-passport.byteimg.com/img/user-avatar/ee5b3d33c959244bf7b70b28bb3a4d07~100x100.awebp" alt={'' /* todo */} loading="lazy" title="用户头像" />
+            <a href="/user/4406498333033918" target="_blank" className={styles.username}>
+              <div className={styles.username}>
+                <span className={styles.name}>天行无忌</span>
+                <span className={styles.rank}>
                   <img src="https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/img/lv-6.b69935b.png" alt={'' /* todo */} title="创作等级" />
                 </span>
               </div>
-              <div className={styles.authorPosition}>全栈开发 @ DevPoint</div>
+              <div className={styles.position}>全栈开发 @ DevPoint</div>
             </a>
           </section>
         </article>
-        <footer className={styles.authorFooter}>完整榜单 todo</footer>
+        <footer className={styles.footer}>完整榜单</footer>
       </div>
     </>
   )
