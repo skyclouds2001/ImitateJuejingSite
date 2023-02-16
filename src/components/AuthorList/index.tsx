f90d9d13830d63fd726f14a246e7a880cf8598dc
skyclouds2001 <skyclouds2001@163.com>
2023-02-16 21:23:33 +0800

    feat: separate author list and author item

--- src/components/AuthorList/index.tsx
+++ src/components/AuthorList/index.tsx
@@ -1,32 +1,7 @@
 import React from 'react'
-import Image from 'next/image'
 import { useAuthor } from '@/api'
-import { type User } from '@/models'
-import { getAuthorWriterLevelImage, customNextImageLoader } from '@/util'
-import styles from './index.module.css'
-
-const AuthorItem: React.FC<{ author: User }> = (props) => {
-  const { author } = props
-
-  return (
-    <>
-      <section>
-        <a className={styles.item} href={`/user/${author.id}`} target="_blank" rel="noreferrer">
-          <Image className={styles.avatar} src={author.profile.url} loader={customNextImageLoader} alt={`${author.username}的头像`} loading="lazy" title="用户头像" width={46} height={46} />
-          <div className={styles.userinfo}>
-            <a href={`/user/${author.id}`} target="_blank" rel="noreferrer" className={styles.username}>
-              <span className={styles.name}>{author.username}</span>
-              <span className={styles.rank}>
-                <Image src={getAuthorWriterLevelImage(author.writerLevel)} alt={`lv-${author.writerLevel}`} title="创作等级" width={35} height={16} />
-              </span>
-            </a>
-            <div className={styles.position}>全栈开发 @ DevPoint</div>
-          </div>
-        </a>
-      </section>
-    </>
-  )
-}
+import styles from './index.module.scss'
+import AuthorItem from '@/components/AuthorItem'
 
 const AuthorList: React.FC = () => {
   const { data: authors } = useAuthor()
