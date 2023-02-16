3c21e1189ca7379b108259793c7fb03b0e1f2871
skyclouds2001 <skyclouds2001@163.com>
2023-02-16 13:58:49 +0800

    feat: remove useless code

--- src/pages/api/hello.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-const handler = (req: NextApiRequest, res: NextApiResponse<{ name: string }>) => {
-  res.status(200).json({ name: 'sky' })
-}
-
-export default handler
--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,17 +1,14 @@
 import { combineReducers } from '@reduxjs/toolkit'
-import helloSlice from './hello'
 import TopTabSlice from './toptab'
 import ArticleTabSlice from './articletab'
 import BookletAdvSlice from './bookletadv'
 
 const rootReducers = combineReducers({
-  hello: helloSlice,
   toptab: TopTabSlice,
   articletab: ArticleTabSlice,
   bookletadv: BookletAdvSlice,
 })
 
-export * from './hello'
 export * from './toptab'
 export * from './articletab'
 export * from './bookletadv'
