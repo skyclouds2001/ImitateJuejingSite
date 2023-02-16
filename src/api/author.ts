00559c39fcfa01288bb59369308ec560796306f0
skyclouds2001 <skyclouds2001@163.com>
2023-02-16 17:14:35 +0800

    fix: update author page

--- src/api/author.ts
+++ src/api/author.ts
@@ -3,9 +3,9 @@ import instance from '@/lib/http'
 import type { User } from '@/models'
 
 export const getAuthor = (): Promise<User[]> => {
-  return instance.get('/api/users')
+  return instance.get('/api/users?populate=*')
 }
 
 export const useAuthor = () => {
-  return useSWR('/api/users', getAuthor)
+  return useSWR('/api/users?populate=*', getAuthor)
 }
