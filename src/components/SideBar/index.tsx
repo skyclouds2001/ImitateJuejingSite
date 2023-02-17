4c364b53d3036c7a8eea6d68437392a4244e40f7
skyclouds2001 <skyclouds2001@163.com>
2023-02-18 01:48:25 +0800

    feat: update sidebar component

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import { useAdvertisement } from '@/api'
 import AuthorList from '@/components/AuthorList'
-import BookletAdv from '@/components/BookletAdv/BookletAdv'
+import Advertisement from '@/components/Advertisement'
+import AppDownload from '@/components/AppDownload'
 import styles from './index.module.css'
 
 const SideBar: React.FC = () => {
+  const { data } = useAdvertisement()
+
   return (
     <>
       <aside className={styles.aside}>
         {/* 小册广告位 */}
-        <BookletAdv />
+        {data?.data.map((v) => (
+          <Advertisement key={v.id} advertisement={Object.assign(v.attributes, { id: v.id })} />
+        ))}
+        {/* 下载 APP 模块 */}
+        <AppDownload />
         {/* 作者榜 */}
         <AuthorList />
       </aside>
