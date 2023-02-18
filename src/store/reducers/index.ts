e22fde6d6a1db3d2f8e81a21308c789a4a41da52
skyclouds2001 <skyclouds2001@163.com>
2023-02-19 02:13:19 +0800

    feat: remove useless store reducer

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,16 +1,13 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import TopTabSlice from './toptab'
 import ArticleTabSlice from './articletab'
-import BookletAdvSlice from './bookletadv'
 
 const rootReducers = combineReducers({
   toptab: TopTabSlice,
   articletab: ArticleTabSlice,
-  bookletadv: BookletAdvSlice,
 })
 
 export * from './toptab'
 export * from './articletab'
-export * from './bookletadv'
 
 export default rootReducers
