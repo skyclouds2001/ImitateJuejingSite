1bb1c2613b910d015d727c234841dbada95c88ed
skyclouds2001 <skyclouds2001@163.com>
2023-02-20 00:31:48 +0800

    feat: update top-tab page

--- src/components/TopTab/index.tsx
+++ src/components/TopTab/index.tsx
@@ -1,16 +1,20 @@
 import React, { useState, useEffect, useContext } from 'react'
+import Link from 'next/link'
 import Image from 'next/image'
 import { CaretDownOutlined } from '@ant-design/icons'
 import { useTopTab } from '@/api'
-import logo from '/public/logo.png'
+import logo from '@/assets/img/logo-pc.svg'
 import { ThemeContext } from '../ThemeContext/ThemeContext'
+import { getTopTabPath } from '@/util'
 import styles from './index.module.css'
 
 const TopTab: React.FC = () => {
   const { data: tabs } = useTopTab()
 
   // 主题 theme
-  const { setTheme } = useContext(ThemeContext)
+  const { theme, setTheme } = useContext(ThemeContext)
+
+  /*
   // 浏览器窗口宽度
   const [Browsewidth, setBrowsWidth] = useState<number>(1400)
   // 浏览器窗口高度
@@ -26,7 +30,7 @@ const TopTab: React.FC = () => {
   const sliderHeightUpdate: any = (e: any) => {
     setSliderHeight(e.target.scrollTop)
     const top = document.querySelector('#' + styles.toptabcontainer)
-    if (top && e.target.scrollTop >= 290) top.className = styles.toptabcontainersroll
+    if (top && e.target.scrollTop >= 500) top.className = styles.toptabcontainersroll
     else if (top) top.className = styles.toptabcontainerfirst
   }
   useEffect(() => {
@@ -40,16 +44,22 @@ const TopTab: React.FC = () => {
       scrollEle.removeEventListener('scroll', sliderHeightUpdate)
     }
   }, [])
+   */
+
   // nav栏较窄的下拉框点击事件
   const handleDropdown = () => {
-    const nav = document.querySelector('.' + styles.navsmall + ' ul')
+    const nav = document.querySelector('.' + styles.narrower + ' ul')
     if (nav) {
       if (nav.className == styles.dropdown) nav.className = styles.hidden
       else nav.className = styles.dropdown
     }
   }
-  // 黑白主题切换按钮点击事件
+
+  /**
+   * 明暗主题切换按钮点击事件
+   */
   const handleThemeChange = () => {
+    console.log(theme)
     if (localStorage.getItem('theme') === 'dark') {
       setTheme('light')
       document.body.style.backgroundColor = '#F4F5F5'
@@ -62,50 +72,54 @@ const TopTab: React.FC = () => {
   return (
     <>
       {/* 顶部tab */}
-      <div id={styles.toptabcontainer} className={styles.toptabcontainerfirst}>
-        {/* 稀土掘金logo区域 */}
-        <div className={styles.logo}>
-          <Image src={logo} alt={'掘金社区'} width={24}></Image>
-          <span>稀土掘金</span>
-        </div>
-        {/* 导航栏 */}
-        <div className={styles.nav}>
-          {/* 页面足够宽-nav导航栏 */}
-          <div className={Browsewidth < 1140 ? styles.hidden : styles.navbig}>
-            <ul>
-              {tabs &&
-                tabs.data.map((item) => (
-                  <li key={item.attributes.key}>
-                    <a href="https://juejin.cn/" className={item.attributes.key === 1 ? styles.curselect : ''}>
-                      {item.attributes.label}
-                    </a>
-                    {/* remark备注 */}
-                    {item.attributes.remark && <span className={styles.remark}>{item.attributes.remark}</span>}
-                  </li>
-                ))}
-            </ul>
+      <header className={styles.container}>
+        <div className={styles.visible}>
+          <div>
+            {/* 稀土掘金logo区域 */}
+            <Link className={styles.logo} href="/" target="_self">
+              <Image src={logo} alt="稀土掘金" width={107}></Image>
+            </Link>
+
+            {/* 导航栏 */}
+            <div className={styles.nav}>
+              {/* 页面足够宽-nav导航栏 */}
+              <div className={styles.wider}>
+                <ul className={styles.list}>
+                  {tabs?.data?.map((v) => (
+                    <li className={styles.item} key={v.attributes.key}>
+                      <a href={getTopTabPath(v.id)} className={v.attributes.key === 0 ? styles.selected : ''}>
+                        {v.attributes.label}
+                      </a>
+                      {v.attributes.remark && <span className={styles.remark}>{v.attributes.remark}</span>}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              {/* 页面不够宽的兼容-nav导航栏 */}
+              <div hidden className={styles.narrower}>
+                <button className={styles.navfirst} onClick={handleDropdown}>
+                  {/*{tabs[0].label}*/}
+                  <CaretDownOutlined />
+                </button>
+                <ul className={styles.hidden}>
+                  {tabs &&
+                    tabs.data.map((item) => (
+                      <li key={item.attributes.key}>
+                        <a href="https://juejin.cn/" className={item.attributes.key === 1 ? styles.curselect : ''}>
+                          {item.attributes.label}
+                        </a>
+                      </li>
+                    ))}
+                </ul>
+              </div>
+            </div>
+
+            {/* 黑白主题切换 */}
+            <button className={styles.theme} onClick={handleThemeChange}></button>
           </div>
-          {/* 页面不够宽的兼容-nav导航栏 */}
-          <div className={(styles.hidden, Browsewidth >= 1140 ? styles.hidden : styles.navsmall)}>
-            <button className={styles.navfirst} onClick={handleDropdown}>
-              {/*{tabs[0].label}*/}
-              <CaretDownOutlined />
-            </button>
-            <ul className={styles.hidden}>
-              {tabs &&
-                tabs.data.map((item) => (
-                  <li key={item.attributes.key}>
-                    <a href="https://juejin.cn/" className={item.attributes.key === 1 ? styles.curselect : ''}>
-                      {item.attributes.label}
-                    </a>
-                  </li>
-                ))}
-            </ul>
-          </div>
         </div>
-        {/* 黑白主题切换 */}
-        <button className={styles.theme} onClick={handleThemeChange}></button>
-      </div>
+      </header>
     </>
   )
 }
