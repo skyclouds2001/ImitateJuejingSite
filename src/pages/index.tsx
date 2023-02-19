f6ad8fe0313a291efb1456806879f23b0ba90815
skyclouds2001 <skyclouds2001@163.com>
2023-02-20 02:10:10 +0800

    feat: update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,10 @@
-import React, { useEffect } from 'react'
-import type { GetServerSideProps } from 'next'
-import { useDispatch } from 'react-redux'
+import React from 'react'
 import ArticleList from '@/components/ArticleList'
-import ArticleTab from '@/components/ArticleTab/ArticleTab'
+import ArticleTab from '@/components/ArticleTab'
 import SideBar from '@/components/SideBar'
-import type { AdvProps } from '@/interface/adv'
-import type { NavProps, ArtNavItem, NavItem, ArtNavProps } from '@/interface/nav'
-import { changeTopNavList, changeArticleNavList, changeAdvList, changeArticleselectkey, type AppDispatch } from '@/store'
 import styles from './index.module.css'
 
-const Home: React.FC<NavProps & AdvProps & ArtNavProps> = (props: NavProps & AdvProps & ArtNavProps) => {
-  const dispatch = useDispatch<AppDispatch>()
-
-  useEffect(() => {
-    // 更新 store 中的 topnav
-    if (props.navList) dispatch(changeTopNavList(props.navList))
-    // 更新 store 中的 artnav
-    if (props.artnavList) dispatch(changeArticleNavList(props.artnavList))
-    // 更新 store 中的 advlist
-    if (props.advList) dispatch(changeAdvList(props.advList))
-    // 更新 store 中的 advlist
-    if (props.selectKey) dispatch(changeArticleselectkey(props.selectKey))
-  }, [dispatch, props])
-
+const Home: React.FC = () => {
   return (
     <>
       <div className={styles.container}>
@@ -38,16 +20,6 @@ const Home: React.FC<NavProps & AdvProps & ArtNavProps> = (props: NavProps & Adv
       </div>
     </>
   )
-}
-
-export const getServerSideProps: GetServerSideProps<NavProps> = async () => {
-  const res = await fetch('http://localhost:3000/api/toptab')
-  const resArt = await fetch('http://localhost:3000/api/articletab')
-  const resAdv = await fetch('http://localhost:3000/api/adv')
-  const data: { navList: NavItem[] } = await res.json()
-  const dataArt: { artnavList: ArtNavItem[]; selectkey: string } = await resArt.json()
-  const dataAdv: { advList: AdvProps } = await resAdv.json()
-  return { props: { navList: data.navList, artnavList: dataArt.artnavList, selectKey: dataArt.selectkey, advList: dataAdv.advList } }
 }
 
 export default Home
